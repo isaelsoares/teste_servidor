a3531f43ab128f063ddb911c06e5e87def0aa624
Isael Soares <isaelsoarees@gmail.com>
2025-09-10 11:31:40 -0300

    teste

--- server.js
+++ server.js
@@ -7,48 +7,58 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-const JWT_SECRET = "super_secret";
+// const JWT_SECRET = "super_secret";
 
 // Rota de login
-app.post("/login", (req, res) => {
-  const { email } = req.body;
 
-  if (!email) {
-    return res.status(400).json({ error: "Email obrigatório" });
-  }
+// app.post("/login", (req, res) => {
+//   const { email } = req.body;
 
-  const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "1h" });
+//   if (!email) {
+//     return res.status(400).json({ error: "Email obrigatório" });
+//   }
 
-  res.json({ token });
-});
+//   const token = jwt.sign({ email }, JWT_SECRET, { expiresIn: "1h" });
 
-// Middleware de autenticação
-function authMiddleware(req, res, next) {
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
+//   res.json({ token });
+// });
 
-  if (!token) return res.sendStatus(401);
+// // Middleware de autenticação
+// function authMiddleware(req, res, next) {
+//   const authHeader = req.headers["authorization"];
+//   const token = authHeader && authHeader.split(" ")[1];
 
-  jwt.verify(token, JWT_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403);
-    req.user = user;
-    next();
-  });
-}
+//   if (!token) return res.sendStatus(401);
 
-// Rota protegida
-app.get("/protected", authMiddleware, (req, res) => {
-  const lista = [
-    { id: 1, nome: "Item 1", descricao: "Primeiro item" },
-    { id: 2, nome: "Item 2", descricao: "Segundo item" },
-    { id: 3, nome: "Item 3", descricao: "Terceiro item" },
-  ];
+//   jwt.verify(token, JWT_SECRET, (err, user) => {
+//     if (err) return res.sendStatus(403);
+//     req.user = user;
+//     next();
+//   });
+// }
 
-  res.json({
-    message: "Acesso autorizado!",
-    user: req.user,
-    data: lista,
-  });
+// // Rota protegida
+// app.get("/protected", authMiddleware, (req, res) => {
+//   const lista = [
+//     { id: 1, nome: "Item 1", descricao: "Primeiro item" },
+//     { id: 2, nome: "Item 2", descricao: "Segundo item" },
+//     { id: 3, nome: "Item 3", descricao: "Terceiro item" },
+//   ];
+
+//   res.json({
+//     message: "Acesso autorizado!",
+//     user: req.user,
+//     data: lista,
+//   });
+// });
+
+app.post("/", async (req, res) => {
+  return res.json({ message: req });
+  // const response = await axios.get("https://hermes.gruposervopa.com.br/apiServopa/SmartShare/GetTempFilesFluxo/2457208", {
+  //   headers: {
+
+  //   }
+  // })
 });
 
 app.listen(3000, () => console.log("Backend rodando na porta 3000"));
